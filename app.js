09d9365c7c077d4ae3011cd9ee9ef91bf69ff1c3
Ahad Hossain <hossain1814@gmail.com>
2021-11-03 15:01:59 +0600

    S-15. Intergration project. Maps in JS. map entry a key and value is an empty array.

--- app.js
+++ app.js
@@ -1,15 +1,21 @@
 // sets in javascipt
 
-const students = new Set(['John', 'Aragon']);
-students.add('Gimli');
-console.log(students);
-console.log(students.has("Gimli"));
-students.delete('John');
-console.log(students);
+// const students = new Set(['John', 'Aragon']);
+// students.add('Gimli');
+// console.log(students);
+// console.log(students.has("Gimli"));
+// students.delete('John');
+// console.log(students);
 
-const studentsArr = Array.from(students);
-console.log(studentsArr);
+// const studentsArr = Array.from(students);
+// console.log(studentsArr);
 
 
-students.clear();
-console.log(students);
+// students.clear();
+// console.log(students);
+
+// Maps in javascript
+
+const sections = new Map([["A2001", []]]);
+
+console.log(sections);
